e1938a4deb4f19acca14c8182520ad556b6cd1ad
dross desktop <daniel.rossos@mail.utoronto.ca>
2020-10-18 02:48:14 -0400

    updated crossover

--- finance_handling.py
+++ finance_handling.py
@@ -1,22 +1,23 @@
 # given in statistical info, dataframe as well as user params to make decision on if to invest and how much
 import numpy as np
 import scipy.stats as stats
-#from stats_functions import ma
+from stats_functions import ma
 
 #get moving average across multiple sized previous periods and dictate current distance fromt he mean
-def movavg_decs(period,funds,curr_value):
-    num_periods = 5
-    len_perioid = period[0:1]
-    historical_ma = list()
-
-    for i in range(num_periods):
-        itter_period = str(len_perioid + len_perioid*i)+period[1:]
-        #historical_ma.append(ma(period=itter_period))
+def movavg_crossover(short_period,long_period,funds,curr_value):
+    #TODO MAKE SURE TO CALL PROPER FUNCTION ONCE IMPLIMENTED
+    long_avg = ma(period=long_period)
+    short_avg = ma(period=short_period)
     
-    potential_gains = curr_value - itter_period[0]
-    z_scores = stats.zscore(historical_ma)
-    std = np.std(historical_ma)
+    buy_flag = long_avg < short_avg
+    potential_gains = curr_value - long_avg
 
+    print(long_period+" Average" + str(long_avg))
+    print(short_period+" Average" + str(short_avg))
     print("Potential Returns: " + str(potential_gains))
     print("Z-Score probability of happening: " + str(z_scores[0]))
+    if(buy_flag):
+        print("BUY")
+    else:
+        print("DONT BUY")
     return
