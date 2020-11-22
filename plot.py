e538a4dc3dd3b34c57d4e028dcb88ce3b0029a28
Maxarreaga <34686490+Maxarreaga@users.noreply.github.com>
2020-11-22 13:05:34 -0500

    used a different library for candlesticks, less trouble

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas_datareader import data as web
 import plotly.graph_objects as go
 from datetime import datetime
 
@@ -49,29 +50,37 @@ def plot_stocks(stocks, time_period):
     
     plt.show()
 
-def plot_candle(stocks,time_period):
+def plot_candle(stocks,start_date):
     
     ticker = yf.Ticker(stocks)
-    df = ticker.history(period = time_period)
-    high_data = df[['High']]
-    open_data = df['Open']
-    low_data = df['Low']
-    close_data = df['Close']
-    #dates = df['Dates']
-    date_list = [datetime.date(datetime.now()) - datetime.timedelta(days=x) for x in range(interval)]
-    date_list.reverse()
+    dfy = ticker.history(period = '1mo')
+    #print(df)
+    high_data = dfy[['High']]
+    open_data = dfy[['Open']]
+    low_data = dfy[['Low']]
+    close_data = dfy[['Close']]
+    #dates = df[['Date']]
+    df = df = web.DataReader(stocks, data_source='yahoo', start=start_date)
+    trace1 = trace1 = {
+    'x': df.index,
+    'open': df.Open,
+    'close': df.Close,
+    'high': df.High,
+    'low': df.Low,
+    'type': 'candlestick',
+    'name': 'MSFT',
+    'showlegend': True
+}
 
-    fig = go.Figure(data=[go.Candlestick(x=dates,
-                        open=open_data, high=high_data,
-                        low=low_data, close=close_data)])
+    fig = go.Figure(data=[trace1])
 
     fig.show()
     
 if __name__ == "__main__":
-    plot_open_close("AAPL", "5d")
+    #plot_open_close("AAPL", "5d")
 
-    plot_high("AAPL", "5d")
+    #plot_high("AAPL", "5d")
 
-    stocks = ["AAPL", "GOOGL", "AMZN", "TSLA"]
-    plot_stocks(stocks, "1mo")
-    #plot_candle("AAPL","1mo") having issues extracting the dates
+    #stocks = ["AAPL", "GOOGL", "AMZN", "TSLA"]
+    #plot_stocks(stocks, "1mo")
+    plot_candle("AAPL",'01-01-2019') #having issues extracting the dates
